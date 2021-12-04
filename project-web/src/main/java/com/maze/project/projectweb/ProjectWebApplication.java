e9695bff8fa206003adecfafa39a3ef043f7c57a
maze <maze1992@126.com>
2021-12-04 15:42:00 +0800

    初始化

---
+++ project-web/src/main/java/com/maze/project/projectweb/ProjectWebApplication.java
@@ -0,0 +1,13 @@
+package com.maze.project.projectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWebApplication.class, args);
+    }
+
+}
---
+++ project-web/src/test/java/com/maze/project/projectweb/ProjectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maze.project.projectweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
