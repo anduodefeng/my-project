11e63ccd0ef92ce0108ea1d5f55f3868b0aa04cc
maze <maze1992@126.com>
2021-12-06 22:37:07 +0800

    add log files

--- project-web/src/main/java/com/maze/project/projectweb/controller/HelloController.java
+++ project-web/src/main/java/com/maze/project/projectweb/controller/HelloController.java
@@ -5,20 +5,20 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.maze.project.projectweb.entity.Person;
 import com.maze.project.projectweb.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RestController
 @RequestMapping("hello")
 public class HelloController {
 
     @Autowired
     private PersonService personService;
-
-
 
     @GetMapping("show")
     public String show(){
@@ -28,6 +28,10 @@ public class HelloController {
 
         Page<Person> page = new Page<>(1,1);
         IPage<Person> ipage = personService.page(page, Wrappers.<Person>lambdaQuery().eq(Person::getName, "张三"));
+
+        log.info("===========打印info日志==============");
+        log.error("===========打印error日志=============");
+
         return "Hello Student!!"+ipage.getRecords();
     }
 }
